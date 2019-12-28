64e58dad7c882281b67098a79b73386de5c19e79
Aditya Misra <amisra14@yahoo.co.uk>
2019-12-29 00:18:29 +0530

    Create test_pyauto__settings.py

---
+++ test_pyauto__settings.py
@@ -0,0 +1,21 @@
+# URLs
+admin_url_from_settings = 'http://*******:*******:redacted/admin/'
+admin_url_without_nginx_authentication = 'http://redacted/admin/'
+
+redacted_site_url_from_settings = 'http://*******:*******:redacted/'
+redacted_site_url_without_nginx_authentication = 'http://redacted/'
+
+# Admin
+admin_user_name = '*******'
+admin_password = '*******'
+
+# Front end
+skip_and_explore_city_name = '*******'
+
+# For testing banner randomization
+setting_banner_type_id = 690
+setting_slide_group_id = 26276
+
+# {dc_id: slide_priority}
+setting_dc_id_and_priority_dict = {102: 1, 2: 3}
+setting_random_dc_id_and_priority_dict = {102: '', 2: 3}
